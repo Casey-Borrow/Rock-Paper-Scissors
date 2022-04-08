62ad901facb88f4dd8961faa363c795dc2501a42
Casey.Borrow <caseyborrow@gmail.com>
2022-04-08 21:56:55 +0100

    Setup flow of trgame round, created function to track scores

--- script.js
+++ script.js
@@ -21,8 +21,6 @@ function computerPlay() {
     }
 }
 
-const computerSelection = computerPlay()
-
 // Ask user for rock, paper or scissorss
     // If user enters anything else reprompt
     // Store player choice in variable 
@@ -46,3 +44,68 @@ const computerSelection = computerPlay()
         // else if player = paper
             // computer wins
         // else draw
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === "rock") {
+        if (playerSelection === "paper") {
+            playerWin();     
+        }
+        else if (playerSelection === "scissors") {
+            computerWin();
+        }
+        else {
+            console.log("Draw");
+        }
+    }
+
+    else if (computerSelection === "paper") {
+        if (playerSelection === "rock") {
+            computerWin();
+        }
+        else if (playerSelection === "scissors") {
+            playerWin();
+        }
+        else {
+            console.log("Draw");
+        }
+    }
+
+    else if (computerSelection === "scissors") {
+        if (playerSelection === "rock") {
+            playerWin();
+        }
+        else if (playerSelection === "paper") {
+            computerWin();
+        }
+        else {
+            console.log("Draw")
+        }
+    }
+}
+
+let computerScore = 0
+let playerScore = 0
+
+function playerWin() {
+    console.log ("Player wins the round");
+    playerScore += 1;
+    currentScore(playerScore, computerScore);
+}
+
+function computerWin() {
+    console.log ("Computer wins the round");
+    computerScore += 1;
+    currentScore(playerScore, computerScore);
+}
+
+function currentScore(playerScore, computerScore) {
+    console.log(`Player: ${playerScore}`);
+    console.log(`Computer: ${computerScore}`);
+}
+
+function game() {
+    for (i = 0; i < 5; i++) {
+        const playerSelection = "rock"
+        const computerSelection = computerPlay();
+    }
+}
