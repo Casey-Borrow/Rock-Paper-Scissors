fb2c56fcfa1f0060d7f22c39ab96d4f57b468efc
Casey.Borrow <caseyborrow@gmail.com>
2022-04-08 20:19:08 +0100

    Setup html and create pseusocode in script

---
+++ script.js
@@ -0,0 +1,33 @@
+// Create computer play
+    // Random number between 1-3
+    // if number = 1
+        // Rock
+    //else if number = 2
+        // Paper
+    // else if num = 3
+        // Scissors 
+    // Stores choice
+
+// Ask user for rock, paper or scissorss
+    // If user enters anything else reprompt
+    // Store player choice in variable
+
+// Compare choices
+    // if computer = rock
+        // if player = paper
+            // player wins
+        // else if player = scissors
+            // computer wins
+        // else draw
+    // else if computer = paper
+        // if player = rock
+            // computer wins
+        // else if player = scissors
+            // player wins
+        // else draw
+    // else computer = scissors
+        // if player = rock
+            // player wins
+        // else if player = paper
+            // computer wins
+        // else draw
